0960276e693aa97898583dd70ad1e199a40d2484
Carlos Eduardo <suportebeloj@gmail.com>
2023-05-11 13:55:12 -0300

    feat(cmd): :sparkles: add settings package

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"github.com/suportebeloj/desafio-hitss/src/cmd/settings"
+	"log"
+)
+
+var (
+	ctx    context.Context = context.Background()
+	dbConn *sql.DB
+)
 
 func main() {
-	fmt.Println("Ola World Go!!")
+	conn, err := settings.NewPostgresConnection()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	defer conn.Close()
+
+	dbConn = conn
+
 }
---
+++ src/cmd/settings/settings.go
@@ -0,0 +1,58 @@
+package settings
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type ICredential interface {
+	GetDSN() string
+}
+
+type PostgresCredential struct {
+	host, user, pass, dbName string
+	port                     int
+}
+
+// function to collect database crendentials from environment
+// and return a DSN string to connection
+func (p *PostgresCredential) GetDSN() string {
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl != "" {
+		return databaseUrl
+	}
+
+	p.host = os.Getenv("DB_HOST")
+
+	dbPort, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+	if dbPort != 0 {
+		p.port = dbPort
+	} else {
+		p.port = 5432
+	}
+
+	p.user = os.Getenv("DB_USER")
+	p.pass = os.Getenv("DB_PASS")
+	p.dbName = os.Getenv("DB_NAME")
+
+	return fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=disable", p.host, p.port, p.dbName, p.user, p.pass)
+}
+
+func NewPostgresConnection() (*sql.DB, error) {
+	postgres := PostgresCredential{}
+	conn, err := sql.Open("postgres", postgres.GetDSN())
+	if err != nil {
+		return nil, err
+	}
+
+	if err := conn.Ping(); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return conn, nil
+}
