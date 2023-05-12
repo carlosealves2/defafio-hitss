1a56fe5b6b18e41d8fd8ed000134c838b5e2634c
Carlos Eduardo <suportebeloj@gmail.com>
2023-05-12 14:02:15 -0300

    feat(encrypter): :sparkles: Text encoder implemented, encryption and decryption function tested

--- src/utils/encrypter/encrypter.go
+++ src/utils/encrypter/encrypter.go
@@ -1,73 +1,169 @@
 package encrypter
 
 import (
-	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
 	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
+const (
+	rsaKeySize = 2048
+)
+
+type keypair struct {
+	priv *rsa.PrivateKey
+	pub  *rsa.PublicKey
+}
+
 type Encrypter struct {
+	kp            keypair
+	signedMessage []byte
+	hashed        [32]byte
+	rng           io.Reader
+	label         string
 }
 
 func NewEncrypter() *Encrypter {
-	return &Encrypter{}
+	instance := &Encrypter{rng: rand.Reader}
+	instance.setup()
+	return instance
 }
 
-func (e Encrypter) Encrypt(plaintext string) (string, error) {
-	key := []byte(os.Getenv("SECRET"))
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return "", err
+func (e *Encrypter) Loadkeys() {
+	keyPath := os.Getenv("KEY_PATH")
+	privKeyPath := fmt.Sprintf("%s/private_key.pem", keyPath)
+	pubKeyPath := fmt.Sprintf("%s/public.pem", keyPath)
+	if _, err := os.Stat(privKeyPath); os.IsNotExist(err) {
+		e.GenerateKeyPair()
+		e.SaveKeyPair()
+		return
 	}
 
-	iv := make([]byte, aes.BlockSize)
-	if _, err := rand.Read(iv); err != nil {
-		return "", err
-	}
-
-	mode := cipher.NewCBCEncrypter(block, iv)
-
-	rawtext := []byte(plaintext)
-
-	// Padding the text with padding to be a multiple of the block size
-	padded := make([]byte, len(rawtext)+(aes.BlockSize-len(rawtext)%aes.BlockSize))
-	copy(padded, rawtext)
-
-	ciphertext := make([]byte, len(padded))
-	mode.CryptBlocks(ciphertext, padded)
-
-	final := append(iv, ciphertext...)
-
-	encoded := base64.StdEncoding.EncodeToString(final)
-	return encoded, nil
+	e.loadPrivateKey(privKeyPath)
+	e.loadPublicKey(pubKeyPath)
 }
 
-func (e Encrypter) Decrypt(encryptedText string) (string, error) {
-	key := []byte(os.Getenv("SECRET"))
+func (e *Encrypter) loadPrivateKey(filename string) error {
+	keyBytes, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
 
-	decoded, err := base64.StdEncoding.DecodeString(encryptedText)
+	block, _ := pem.Decode(keyBytes)
+	if block == nil {
+		return errors.New("failed to decode PEM block containing private key")
+	}
+
+	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return err
+	}
+	e.kp.priv = key
+
+	return nil
+}
+
+func (e *Encrypter) loadPublicKey(filename string) (*rsa.PublicKey, error) {
+	keyBytes, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(keyBytes)
+	if block == nil {
+		return nil, errors.New("failed to decode PEM block containing public key")
+	}
+
+	key, err := x509.ParsePKCS1PublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+func (e *Encrypter) GenerateKeyPair() error {
+	var err error
+
+	e.kp.priv, err = rsa.GenerateKey(rand.Reader, rsaKeySize)
+	if err != nil {
+		return err
+	}
+
+	e.kp.pub = &e.kp.priv.PublicKey
+	return nil
+}
+
+func (e *Encrypter) SaveKeyPair() error {
+	keyPath := os.Getenv("KEY_PATH")
+	privKeyPath := fmt.Sprintf("%s/private_key.pem", keyPath)
+	pubKeyPath := fmt.Sprintf("%s/public.pem", keyPath)
+
+	keyBytes := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(e.kp.priv),
+	})
+
+	pubKeyBytes, err := x509.MarshalPKIXPublicKey(&e.kp.priv.PublicKey)
+	if err != nil {
+		return err
+	}
+
+	pubKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: pubKeyBytes,
+	})
+
+	if err := os.WriteFile(privKeyPath, keyBytes, 0600); err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(pubKeyPath, pubKeyPEM, 0644); err != nil {
+		return err
+	}
+
+	log.Println("private and public key generated and saved")
+
+	return nil
+}
+
+func (e *Encrypter) Encrypt(plainText string) (string, error) {
+	ciphertext, err := rsa.EncryptOAEP(sha256.New(), e.rng, e.kp.pub, []byte(plainText), []byte(e.label))
+	encode := base64.StdEncoding.EncodeToString(ciphertext)
+	if err != nil {
+		return "", err
+	}
+	return encode, nil
+}
+
+func (e *Encrypter) Decrypt(encode string) (string, error) {
+	decode, err := base64.StdEncoding.DecodeString(encode)
+	if err != nil {
+		return "", err
+	}
+	plaintext, err := rsa.DecryptOAEP(sha256.New(), e.rng, e.kp.priv, decode, []byte(e.label))
 	if err != nil {
 		return "", err
 	}
 
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return "", err
+	return string(plaintext), nil
+}
+
+func (e *Encrypter) setup() {
+	e.Loadkeys()
+	label := os.Getenv("SECRET_PHRASE")
+	if label == "" {
+		log.Fatalln("secret phrase not set on environment")
+		return
 	}
 
-	iv := decoded[:aes.BlockSize]
-	ciphertext := decoded[aes.BlockSize:]
-
-	mode := cipher.NewCBCDecrypter(block, iv)
-
-	plaintext := make([]byte, len(ciphertext))
-	mode.CryptBlocks(plaintext, ciphertext)
-
-	// Remove padding from text
-	unpadded := bytes.TrimRight(plaintext, "\x00")
-	return string(unpadded), nil
+	e.label = label
 }
--- src/utils/encrypter/encrypter_test.go
+++ src/utils/encrypter/encrypter_test.go
@@ -1,59 +1,40 @@
 package encrypter_test
 
 import (
-	"crypto/rand"
 	"encoding/base64"
 	"github.com/stretchr/testify/assert"
 	"github.com/suportebeloj/desafio-hitss/src/utils/encrypter"
-	"os"
 	"testing"
 )
 
-func init() {
-	key := generateFakeKey()
-	os.Setenv("SECRET", string(key))
-}
-
-func generateFakeKey() (key []byte) {
-	const keySize = 32
-
-	key = make([]byte, keySize)
-	if _, err := rand.Read(key); err != nil {
-		panic(err)
-	}
-
-	return
-}
-
-func TestEncrypter_Encrypt_AString_WithSuccess_AndReturnABase64String(t *testing.T) {
-	testPhrase := "plain text string"
+func TestEncrypter_Setup(t *testing.T) {
 	instance := encrypter.NewEncrypter()
-	encoded, err := instance.Encrypt(testPhrase)
-	assert.NoError(t, err)
+	assert.NotNil(t, instance)
+}
 
-	//	check if encoded string is base64
+func TestEncrypter_Encrypt_AString_WithSuccess(t *testing.T) {
+	textToEncrypt := "encrypt text to test"
+
+	instance := encrypter.NewEncrypter()
+	assert.NotNil(t, instance)
+	encoded, err := instance.Encrypt(textToEncrypt)
+	assert.NoError(t, err)
 	_, err = base64.StdEncoding.DecodeString(encoded)
 	assert.NoError(t, err)
 }
 
-func TestEncrypter_Decrypt_ABase64String_WithSuccess(t *testing.T) {
-	testPhrase := "plain text string"
+func TestEncrypter_Decrypt_AString_WithSuccess(t *testing.T) {
+	textToEncrypt := "encrypt text to test"
+
 	instance := encrypter.NewEncrypter()
-	encoded, err := instance.Encrypt(testPhrase)
+	assert.NotNil(t, instance)
+	encoded, err := instance.Encrypt(textToEncrypt)
+	assert.NoError(t, err)
+	_, err = base64.StdEncoding.DecodeString(encoded)
 	assert.NoError(t, err)
 
-	decoded, err := instance.Decrypt(encoded)
+	plainText, err := instance.Decrypt(encoded)
 	assert.NoError(t, err)
-	assert.Equal(t, decoded, testPhrase)
-}
+	assert.Equal(t, plainText, textToEncrypt)
 
-func TestEncrypter_Decrypt_GivenAnError_WhenTryDecrypt_UsingAIvalidHash(t *testing.T) {
-	testPhrase := "plain text string"
-	instance := encrypter.NewEncrypter()
-	encoded, err := instance.Encrypt(testPhrase)
-	assert.NoError(t, err)
-
-	encoded = string([]byte(encoded)[3:])
-	_, err = instance.Decrypt(encoded)
-	assert.Error(t, err)
 }
